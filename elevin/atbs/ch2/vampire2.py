be4d161de6da359e0d68eefb4eb3dc1d49802500
elevin <elevin@ixl.com>
2020-05-08 12:15:53 -0700

    el ch2 exercises

--- elevin/atbs/ch2/vampire2.py
+++ elevin/atbs/ch2/vampire2.py
@@ -4,7 +4,7 @@ if name == 'Alice':
     print('Hi, Alice.')
 elif age < 12:
     print('You are not Alice, kiddo.')
-elif age > 2000:
-    print('Unlike you, Alice is not an undead, immortal vampire.')
 elif age > 100:
-    print('You are not Alice, grannie.')
+    print('You are note Alice, grannie.')
+elif age > 200:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
