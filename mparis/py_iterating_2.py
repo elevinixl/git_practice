286f7906157f8cb135dcf5a5363b8313ec421d72
mparisixl <mparis@ixl.com>
2020-07-10 11:07:23 -0700

    Added iterate2 file update

--- mparis/py_iterating_2.py
+++ mparis/py_iterating_2.py
@@ -1,18 +1,21 @@
-ex=[2,5,3,4,4,7,1,10]
-ex2=['Fivee','Four','Sevennn','Tu','Nineeeeee']
+#consider renaming variables in ways that make it clear if its explicitly expecting a number or string
+
+ex=[2,7,-2,3,4,4,5,1,10]
+ex2=['Fivee','Four','Sevennn','Tu','Nineeeeee','Short']
 ex3=4
 #-------------------------------------------------------------------------------------
 #1a
 def listResult(ex):
     lowest = ex[0]
     for x in ex:
-        if x < ex[0]:
+        if x < ex[0]: #Won't yield correct result in some cases
             lowest = x
     return lowest
 print('[1-A] Lowest number in list is: '+ str(listResult(ex)))
 #1b
+
 def listResult(ex):
-    lowest = 999999
+    lowest = ex[0]  #Add validation check that list at least has one element
     for x in ex:
         if x < lowest:
             lowest = x
@@ -21,20 +24,20 @@ print('[1-B] Lowest number in list is: '+ str(listResult(ex)))
 
 #-------------------------------------------------------------------------------------
 #2a
-def listResult(ex2):
-    largest = ex[0]
+def listResult(ex2): #Incorrect, returns x, which is always last value
+    largest = ex2[0]
     for x in ex2:
-        if len(x) > ex[0]:
+        if len(x) > len(ex2[0]):
             largest = len(x)
     return x
 print(' [2-A] Longest word in list is: '+ str(listResult(ex2)))
 #2b
-def listResult(ex2):
-    largest = len(ex2[0])
+def listResult(ex2): #Correct, returns largest value
+    largest = ex2[0]
     for x in ex2:
-        if len(x) > largest:
-            largest = len(x)
-    return x
+        if len(x) > len(largest):
+            largest = x
+    return largest
 print(' [2-B] Longest word in list is: '+ str(listResult(ex2)))
 
 #-------------------------------------------------------------------------------------
@@ -59,11 +62,11 @@ print('    [4-A] Numbers in list that are greater than (4): '+ str(listResult(ex
 #-------------------------------------------------------------------------------------
 #5a
 def listResult(ex,ex3):
-    result = []
+    result = []  #count = 0
     for x in ex:
         if x == ex3:
-            result = result+[x]
-    return len(result)
+            result = result+[x] #count += 1
+    return len(result) #count
 print('     [5-A] Number of times (4) displays in list: '+ str(listResult(ex,ex3)))
 
 #-------------------------------------------------------------------------------------
@@ -71,10 +74,10 @@ print('     [5-A] Number of times (4) displays in list: '+ str(listResult(ex,ex3
 def listResult(ex):
     result = []
     for x in ex:
-        if x > -999999:
-            result = result + [x+1]
+        result = result + [x+1]
     return result
 print('      [6-A] Each item in list +1: '+ str(listResult(ex)))
+
 
 
 
